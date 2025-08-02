7a2cda1be172e397931ab45970b07ca12de290d1
Michalis Karatsioris <mkaratsioris@yahoo.com>
2025-08-02 12:48:59 +0200

    add second stage - respond with 200

---
+++ http_server/1-stages/2_respond_with_200.py
@@ -0,0 +1,31 @@
+import socket  # noqa: F401
+
+def handle_request(client_socket):
+    request: str = client_socket.recv(1_024).decode()
+    print(f"Request:\n{request}")
+    status: str = "HTTP/1.1 200 OK"
+    headers: str = ""
+    body: str = ""
+    response: str = "\r\n".join([status, headers, body])
+    client_socket.sendall(response.encode())
+
+
+def main():
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    print("HTTP Server is running on localhost:4221...")
+    try:
+        while True:
+            print("Waiting for a connection...")
+            client_socket, client_address = server_socket.accept()
+            print(f"Connection established with client ({client_address})")
+            handle_request(client_socket=client_socket)
+            client_socket.close()
+    except KeyboardInterrupt as e:
+        print("HTTP Server is shutting down...")
+    finally:
+        server_socket.close()
+        print("HTTP Serber has been shut down successfully!")
+
+
+if __name__ == "__main__":
+    main()
