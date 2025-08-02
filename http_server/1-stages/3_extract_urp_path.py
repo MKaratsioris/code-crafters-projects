a902932ce857dc5a5f0916b26999d7b500fcd9f5
Michalis Karatsioris <mkaratsioris@yahoo.com>
2025-08-02 12:49:40 +0200

    add third stage - extract url path

---
+++ http_server/1-stages/3_extract_urp_path.py
@@ -0,0 +1,53 @@
+from socket import socket, create_server
+
+# TODO Create a class to encapsulate the functionality
+
+def parse_request(data: str) -> tuple[str, str, str]:
+    """ """
+    lines: list[str] = data.split('\r\n')
+    print(f"\tRequest:")
+    for line in lines:
+        print(f"\t\t{line}")
+    first_line: str = lines[0]
+    method, path, version = first_line.split(' ')
+    return method, path, version
+
+def get_response(path: str) -> str:
+    """ """
+    response_status_dict: dict[str, str] = {
+        "/": "HTTP/1.1 200 OK",
+    }
+    default_response_status: str = "HTTP/1.1 404 Not Found"
+    return response_status_dict.get(path, default_response_status)
+
+def handle_request(client_socket: socket) -> None:
+    """ """
+    request: str = client_socket.recv(1_024).decode()
+    _, path, _ = parse_request(data=request)
+    status: str = get_response(path=path)
+    headers: str = ""
+    body: str = ""
+    response: str = "\r\n".join([status, headers, body])
+    print(f"\tResponse:\n\t\t{response}")
+    client_socket.sendall(response.encode())
+
+
+def main():
+    server_socket = create_server(("localhost", 4221), reuse_port=True)
+    print("[HTTP Server] Running on localhost:4221")
+    try:
+        while True:
+            print("[HTTP Server] Listening for connections...\n")
+            client_socket, client_address = server_socket.accept()
+            print(f"[HTTP Server] Connection established with client {client_address[0]}:{client_address[1]}")
+            handle_request(client_socket=client_socket)
+            client_socket.close()
+    except KeyboardInterrupt as e:
+        print("[HTTP Server] Shutting down...")
+    finally:
+        server_socket.close()
+        print("[HTTP Server] Shut down successfully!")
+
+
+if __name__ == "__main__":
+    main()
