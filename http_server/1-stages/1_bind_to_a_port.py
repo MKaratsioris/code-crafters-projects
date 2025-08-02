71bb8969913f195764ee00703e016c3d520715e1
Michalis Karatsioris <mkaratsioris@yahoo.com>
2025-08-02 12:48:40 +0200

    add first stage - bind to a port

---
+++ http_server/1-stages/1_bind_to_a_port.py
@@ -0,0 +1,19 @@
+import socket  # noqa: F401
+
+def main():
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    print("HTTP Server is running on localhost:4221")
+    try:
+        while True:
+            print("Waiting for a connection...")
+            client_socket, client_address = server_socket.accept()
+            print(f"Connection established with client: {client_address}")
+            client_socket.close()
+    except KeyboardInterrupt as e:
+        print("HTTP Server is shutting down")
+    finally:
+        server_socket.close()
+        print("HTTP Server has been shut down successfully")
+
+if __name__ == "__main__":
+    main()
